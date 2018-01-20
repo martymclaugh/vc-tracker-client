b350af409dbe878414c71aae7d0300a3e4a5b347
Marty McLaughlin <martymclaugh@gmail.com>
2018-01-20 15:49:40 -0800

    add vc inputs and input state

---
+++ src/helpers/vc-inputs.js
@@ -0,0 +1,78 @@
+export const initialFormState = {
+  name: null,
+  affiliation: null,
+  website: null,
+  contact: null,
+  check_size: null,
+  investments_per_year: null,
+  status: null,
+  location: null,
+  potential: null,
+  interests: null,
+};
+
+export const vcInputs = {
+  name: {
+    field: 'name',
+    placeholder: 'Venture Capitalist Name/Firm',
+  },
+  affiliation: {
+    field: 'affiliation',
+    placeholder: 'Affiliation',
+    dropdown: true,
+    options: [
+      'Individual',
+      'Group',
+      'Venture Firm'
+    ],
+  },
+  website: {
+    field: 'website',
+    placeholder: 'Website',
+  },
+  contact: {
+    field: 'contact',
+    placeholder: 'Contact',
+  },
+  check_size: {
+    field: 'check_size',
+    placeholder: 'Typical Check Size',
+    prefix: '$',
+    type: 'number',
+  },
+  investments_per_year: {
+    field: 'investments_per_year',
+    placeholder: 'Investments Per Year',
+    type: 'number',
+  },
+  status: {
+    field: 'status',
+    placeholder: 'Status',
+    dropdown: true,
+    options: [
+      'Contacted',
+      'Researched',
+      'Acquired',
+      'Discussing Terms',
+    ],
+  },
+  location: {
+    field: 'location',
+    placeholder: 'Location',
+  },
+  potential: {
+    field: 'potential',
+    placeholder: 'Potential',
+    dropdown: true,
+    options: [
+      'High',
+      'Medium',
+      'Low',
+    ],
+  },
+  interests: {
+    field: 'interests',
+    placeholder: 'Interests',
+    textArea: true,
+  },
+};
