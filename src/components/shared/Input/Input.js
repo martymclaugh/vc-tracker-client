92dfebf60b878053dc66d3116d60add7d2276aa6
Marty McLaughlin <martymclaugh@gmail.com>
2018-01-19 19:59:26 -0800

    add input component

---
+++ src/components/shared/Input/Input.js
@@ -0,0 +1,28 @@
+import React from 'react';
+const renderInput = (props) => {
+  console.log(props.field, props.type);
+  if (props.textArea) {
+    return (
+      <textarea cols="30" rows="5"
+        className={`input input__${props.field}`}
+        placeholder={props.placeholder}
+        onChange={(e) => props.handleKeyPress(e, props.field)}
+      />
+    );
+  }
+  return (
+    <input
+      className="input"
+      placeholder={props.placeholder}
+      onChange={(e) => props.handleKeyPress(e, props.field)}
+      type={props.type || 'text'}
+    />
+  );
+}
+export default (props) => (
+  <div className="input__container">
+    {props.title && <div className="input__title">{props.title}</div>}
+    {props.prefix && <span className="prefix">{props.prefix}</span>}
+    {renderInput(props)}
+  </div>
+)
