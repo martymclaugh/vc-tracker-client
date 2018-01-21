75d93610bf091fd7146be5840456b51704b4d43f
Marty McLaughlin <martymclaugh@gmail.com>
2018-01-21 13:39:05 -0800

    format company list

--- src/components/shared/CompanyList/CompanyList.js
+++ src/components/shared/CompanyList/CompanyList.js
@@ -1,22 +1,27 @@
 // @flow
 
 import React from 'react';
+import toCurrency from '../../../helpers/to-currency';
+import formatDate from '../../../helpers/format-date';
 import { Link } from 'react-router-dom';
 import { Props } from '../../../flow/shared/company-list-types';
+
+import './company-list-styles.css';
 
 const renderCompanies = (companies) => {
   if (companies) {
     return companies.map(company => (
-      <Link to={`/companies/${company.slug}`}>
-        <div key={company.slug} className="company/">
-          <span>{company.name}  </span>
-          <span>{company.raised}  </span>
-          <span>{company.timeline}</span>
-        </div>
-      </Link>
+      <Link
+        key={company.slug}
+        className="company-list__item"
+        to={`/companies/${company.slug}`}
+      >
+          <span className="company__title">{company.name}</span>
+          <span className="company__raised">{toCurrency(company.raised)}  </span>
+          <span className="company__timeline">Days Left: {formatDate(company.timeline).days}</span>
+        </Link>
     ));
   }
-
   return null;
 }
 
